5fe13a9a7e0cf933c95b9196198673c0b7778754
Tyrant Davis <davistyrant@gmail.com>
2017-10-13 12:39:42 -0500

    Creates directory. Adds intro_complexity_answers.txt and iterations.rb. Assignment complete.

---
+++ 03-complexity/iterations.rb
@@ -0,0 +1,12 @@
+# iterations.rb
+
+def iterations(n)
+  puts "Worst-Case Linear Search\v"
+  print "n"
+  puts "    # Iterations O(n)\v"
+  (1..n).each do |n|
+    puts "#{n}\t #{n} "
+  end
+end
+
+iterations(10)
