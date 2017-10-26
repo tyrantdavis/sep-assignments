e879df252b16fe13754c6b96949907759d4ac7fd
Tyrant Davis <davistyrant@gmail.com>
2017-10-26 11:57:55 -0500

    Modifies #initialize. Creates #films_and_actors

--- 01-data-structures/07-graphs/node.rb
+++ 01-data-structures/07-graphs/node.rb
@@ -1,12 +1,24 @@
 class Node
   attr_accessor :name
   attr_accessor :film_actor_hash
-  attr_accessor :films
-  attr_accessor :viewed
+  attr_accessor :films_array
+  attr_accessor :tested
 
-  def initialize
+  def initialize(name)
     @name = name
-    @films = []
-    @viewed = false
+    @films_array = []
+    @tested = false
+  end
+
+  def films_and_actors(films)
+    @film_actor_hash = Hash.new
+    films.each do |key, value|
+      value.each do |node|
+        if node.name == name
+          @film_actor_hash[key] = value
+          @film_actor_hash[key] -= [node]
+        end
+      end
+    end
   end
 end
