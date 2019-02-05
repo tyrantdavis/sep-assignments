6298dee0802ad0f0a7538ce116c56dce8ddd4533
Tyrant Davis <davistyrant@gmail.com>
2019-02-05 16:20:07 -0600

    Update binary search.

--- 02-algoritms/searching/binary_search_iterative.rb
+++ 02-algoritms/searching/binary_search_iterative.rb
@@ -1,22 +1,25 @@
-def binary_search(collection, value)
+def binary_search(collection, target)
   low = 0
   high = collection.length - 1
   mid = collection.length / 2
 
   while low <= high
     mid = (low + high) / 2
-
-    if collection[mid] > value
+    if collection[mid] > target
       high = mid - 1
-    elsif collection[mid] < value
+    elsif collection[mid] < target
       low = mid + 1
     else
-      return "search returned #{collection[mid]} @ index #{mid}"
+      return "Search returned target: #{collection[mid]} @ index #{mid}"
     end
   end
-  return "value: #{value} not found"
+  return "Target: #{target} not found"
 end
 
 ary = ['a','b','c','d','e','f','g']
+ary2 = ['g','r','e','a','b','f','z']
 puts binary_search(ary, 'e')
-puts binary_search(ary, 'z')
+puts
+puts binary_search(ary2.sort, 'z')
+puts
+puts binary_search(ary2.sort, 'c')
